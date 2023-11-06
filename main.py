caca87db665da9e6a50f2327e6d0e5e5e32a73e0
Gatien DA ROCHA <da-rocha.e2200358@etud.univ-ubs.fr>
2023-11-06 14:14:45 +0100

    Repertoire tmp au démarrage de l'application

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import logging
+import os
 from PyQt6.QtWidgets import QApplication
 from views.start_view import StartView
 
 # Configure logging
 log_format = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=log_format)
+
+if not os.path.exists('tmp'):
+    os.mkdir('tmp')
 
 # Set up the QApplication
 app = QApplication([])
