84920b64e2c12d0b4d7be19566ee6ecbfb3c85e9
Gatien <gatiendr@gmail.com>
2023-11-13 12:27:10 +0100

    Document url_handler.py

--- utils/url_handler.py
+++ utils/url_handler.py
@@ -1,6 +1,12 @@
 import requests
 
 def get_content_type(url: str) -> str | None:
+    """
+    Gets the content type of a URL
+    :param url: URL to check
+    :return: Content type of the URL or None if the request failed or the content type is not found
+    """
+
     try:
         response = requests.head(url, allow_redirects=True, timeout=10)
         return response.headers.get('Content-Type', '').split(';')[0].strip()
@@ -8,6 +14,12 @@ def get_content_type(url: str) -> str | None:
         return None
 
 def is_image(url: str) -> bool:
+    """
+    Convenience function to check if a URL is an image
+    :param url: URL to check
+    :return: True if the URL points to what is supposed to be an image, False otherwise
+    """
+
     content_type = get_content_type(url)
     if not content_type:
         return False
@@ -15,6 +27,12 @@ def is_image(url: str) -> bool:
     return content_type in image_types
 
 def is_video(url: str) -> bool:
+    """ 
+    Convenience function to check if a URL is a video
+    :param url: URL to check
+    :return: True if the URL points to what is supposed to be a video, False otherwise
+    """
+
     content_type = get_content_type(url)
     if not content_type:
         return False
@@ -23,6 +41,12 @@ def is_video(url: str) -> bool:
     return content_type in video_types
 
 def is_live_video(url: str) -> bool:
+    """
+    Convenience function to check if a URL is a live video
+    :param url: URL to check
+    :return: True if the URL points to what is supposed to be a live video, False otherwise
+    """
+
     content_type = get_content_type(url)
     live_content_types = ["application/vnd.apple.mpegurl", "application/dash+xml"]
     live_url_patterns = ["m3u8", ".ts", "live", "streaming"]
@@ -31,3 +55,4 @@ def is_live_video(url: str) -> bool:
     if any(pattern in url for pattern in live_url_patterns):
         return True
     return False
+
