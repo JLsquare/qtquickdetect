11968144153f885bd571cfe1432358946405bc42
JL <jeanloup.mellion@gmail.com>
2023-11-15 15:28:54 +0100

    SAE-89 bouton save json et image avec les layer d'activer

--- views/image_result_widget.py
+++ views/image_result_widget.py
@@ -1,13 +1,14 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QHBoxLayout, QPushButton, QGraphicsPixmapItem, \
-    QGraphicsScene, QComboBox, QLabel, QListWidget, QListWidgetItem
-from PyQt6.QtGui import QPixmap, QImage
-from PyQt6.QtCore import Qt
+    QGraphicsScene, QComboBox, QLabel, QListWidget, QListWidgetItem, QFileDialog, QMessageBox
+from PyQt6.QtGui import QPixmap, QImage, QPainter
+from PyQt6.QtCore import Qt, QFile
 from models.app_state import AppState
 from utils.image_helpers import draw_bounding_box
 from views.resizeable_graphics_widget import ResizeableGraphicsWidget
 import json
 import os
 import numpy as np
+import logging
 
 appstate = AppState.get_instance()
 
@@ -137,10 +138,51 @@ class ImageResultWidget(QWidget):
     ##############################
 
     def save_image(self):
-        pass
+        base_image_path = self._input_images[self._file_select_combo.currentIndex()]
+        base_pixmap = QPixmap(base_image_path)
+        canvas = QPixmap(base_pixmap.size())
+        canvas.fill(Qt.GlobalColor.transparent)
+
+        painter = QPainter(canvas)
+        painter.drawPixmap(0, 0, base_pixmap)
+
+        for (class_id, index), layer_info in self._layer_visibility.items():
+            if layer_info['visible']:
+                layer_item = layer_info['item']
+                layer_pixmap = layer_info['initial_pixmap']
+                pos = layer_item.pos()
+                painter.drawPixmap(pos, layer_pixmap)
+
+        painter.end()
+
+        if appstate.config.image_format == 'png':
+            file_name, selected_filter = QFileDialog.getSaveFileName(self, "Save PNG", "", "PNG (*.png)")
+        else:
+            file_name, selected_filter = QFileDialog.getSaveFileName(self, "Save JPEG", "", "JPEG (*.jpg)")
+        if file_name:
+            if selected_filter == "PNG (*.png)" and not file_name.lower().endswith('.png'):
+                file_name += ".png"
+            elif (selected_filter == "JPEG (*.jpg)" and not file_name.lower().endswith('.jpg')
+                  and not file_name.lower().endswith('.jpeg')):
+                file_name += ".jpg"
+            if canvas.save(file_name):
+                QMessageBox.information(self, "Success", "Image saved successfully!")
+                logging.debug(f'Saved image to {file_name}')
+            else:
+                QMessageBox.critical(self, "Error", "An error occurred while saving the image.")
+                logging.error(f'Could not save image to {file_name}')
 
     def save_json(self):
-        pass
+        file_name, selected_filter = QFileDialog.getSaveFileName(self, "Save JSON", "", "JSON (*.json)")
+        if file_name:
+            if not file_name.lower().endswith('.json'):
+                file_name += ".json"
+            if QFile.copy(self._result_jsons[self._file_select_combo.currentIndex()], file_name):
+                QMessageBox.information(self, "Success", "JSON saved successfully!")
+                logging.debug(f'Saved JSON to {file_name}')
+            else:
+                QMessageBox.critical(self, "Error", "An error occurred while saving the JSON.")
+                logging.error(f'Could not save JSON to {file_name}')
 
     def add_input_and_result(self, input_image: str, result_json: str):
         self._input_images.append(input_image)
@@ -186,6 +228,7 @@ class ImageResultWidget(QWidget):
         if layer_info:
             layer_info['item'].setVisible(item.checkState() == Qt.CheckState.Checked)
             layer_info['visible'] = layer_info['item'].isVisible()
+            layer_info['initial_pixmap'] = layer_info['item'].pixmap()
 
     def draw_bounding_box_on_layer(self, scene, result, img_size, index, classes):
         # Utility function for drawing bounding boxes
@@ -201,7 +244,8 @@ class ImageResultWidget(QWidget):
                           config.video_box_color, config.video_text_color,
                           config.video_box_thickness, config.video_text_size)
 
-        q_img = QImage(layer.data, img_size.width(), img_size.height(), 4 * img_size.width(), QImage.Format.Format_RGBA8888)
+        q_img = QImage(layer.data, img_size.width(), img_size.height(), 4 * img_size.width(),
+                       QImage.Format.Format_RGBA8888)
         layer_pixmap = QPixmap(q_img)
         layer_item = QGraphicsPixmapItem(layer_pixmap)
         scene.addItem(layer_item)
@@ -209,5 +253,6 @@ class ImageResultWidget(QWidget):
         # Update layer visibility mapping
         self._layer_visibility[(result['classid'], index)] = {
             'item': layer_item,
-            'visible': True
+            'visible': True,
+            'initial_pixmap': layer_pixmap
         }
